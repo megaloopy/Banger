bf55d5ffecb4d4e3a58396fa59a4af3d2af8b541
megaloopy <pixelpedro@gmail.com>
2017-05-17 04:57:42 +0000

    Boostrap flash msgs + links authentication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+   
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when "notice"
+        "alert-success"   # Green
+      when "alert"
+        "alert-danger"    # Red
+      # when "alert"
+      #  "alert-warning"   # Yellow
+      # when "info"
+      #  "alert-info"      # Blue
+      else
+        flash_type.to_s
+    end
+  end
+  
 end
