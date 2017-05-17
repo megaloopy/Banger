4c9a0d02d553aebec53a656c96ec3f7b1f07440d
megaloopy <pixelpedro@gmail.com>
2017-05-17 03:09:51 +0000

    Work on CRUD actions for vehivle model

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -14,7 +14,7 @@ class VehiclesController < ApplicationController
       redirect_to @vehicle
     else
       render 'new'
-    end  
+    end
   end
 
   def show
@@ -22,12 +22,25 @@ class VehiclesController < ApplicationController
   end
 
   def edit
+    @vehicle = Vehicle.find(params[:id])
   end
 
   def update
+    @vehicle = Vehicle.find(params[:id])
+    if @vehicle.update(vehicle_params)
+      redirect_to @vehicle
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @vehicle = Vehicle.find(params[:id])
+    if @vehicle.delete
+      redirect_to root_path
+    else 
+      render :show
+    end
   end
   
   private
