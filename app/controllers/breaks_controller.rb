8b883da8ed7394b443044ac97d4327e0200db5f9
megaloopy <pixelpedro@gmail.com>
2017-06-20 11:25:23 +0000

    breaks new, nick name display, link car pic to service

--- app/controllers/breaks_controller.rb
+++ app/controllers/breaks_controller.rb
@@ -1,2 +1,35 @@
 class BreaksController < ApplicationController
+  
+  def new
+    @service = Service.find(params[:id])
+    @break = Break.new
+  end
+  
+  def create
+    @service = Service.find(params[:id])
+    @break = Break.build(break_params)
+      if @break.save
+        redirect_to @service
+      else
+        render 'new'
+      end    
+  end
+  
+  def edit
+    
+  end
+  
+  def show
+    @break = Break.find(params[:id])
+  end
+  
+  def index
+    @breaks = Break.all
+  end
+  
+  private
+  
+  def break_params
+    params.require(:brake).permit(:brand, :front, :rear)
+  end
 end
