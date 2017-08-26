fb91eaaeff2b361ed32b9c82ce4169fdeb41e462
megaloopy <pixelpedro@gmail.com>
2017-08-26 14:59:40 +0000

    working on carousel slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require bootstrap
 //= require jquery_ujs
 //= require turbolinks
+//= require slider
 //= require_tree .
---
+++ app/assets/javascripts/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.main-slider').slick({
+    accessibility: true,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+});
+
