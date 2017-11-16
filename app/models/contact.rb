17165e5547ee7bd93d21e775a32757be11b64de5
megaloopy <pixelpedro@gmail.com>
2017-11-16 10:32:55 -0500

    Unsplash api finally working

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < MailForm::Base
   
-   append :remote_ip, :user_agent
+  append :remote_ip, :user_agent
   attribute :name,      validate: true
   attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
